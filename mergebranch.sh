035bb6a4b8a026c6ebb374d2794df75d8c9d9a84
Marcos Pivetta <m.pivetta@centauro.com.br>
2017-04-25 17:31:08 -0300

    Nova versão do mergebranch

--- mergebranch.sh
+++ mergebranch.sh
@@ -2,55 +2,70 @@
 
 set +x
 
-email="m.pivetta@centauro.com.br"
-name="Pivetta"
+declare -a conflitos=()
+declare -a semconflitos=()
 
-git config --global user.email $email
-git config --global user.name $name
-git config --global push.default simple
+#Configura o push como simple
+git config --global push.default simple >> /dev/null
 
-if [[ $(git config user.email) == $email || $(git config user.name) == $name ]]; 
-	then
-		echo "======================================================================="
-		echo "Usuário e senha global do git estão corretos:"
-		echo "email : $(git config user.email)"
-		echo "usuário : $(git config user.name)"
-	else
-		echo "======================================================================="
-		echo "E-mail e usuário global do GIT estão incorretos:"
-		echo "email : $(git config --global user.email $email)"
-		echo "usuário : $(git config --global user.name $name)"
-fi
-		echo "======================================================================="
-
-#Faz checkout da "master" e atualização "master" local
+#Faz checkout da "master" e atualização "master" local.
+echo -e "\n======================================================================="
+echo "Atualizando a master"
+echo -e "=======================================================================\n"
+#git config remote.origin.url http://m.pivetta:gravedigger@tfs.dc.sbnet/tfs/Centauro/Retail%20Offline/_git/sispac
 git checkout master
 git fetch -p
 git pull
 
-#Cria variável local "branches" com todas as branches remotas (exceto a "master")
+echo -e "\n======================================================================="
+echo "Atualizando branches | Total de branches: " $(git branch -r | egrep -v 'master' | cut -c 10- | wc -l)
+echo -e "=======================================================================\n"
+#Cria variável local "branches" com todas as branches remotas (exceto a 'master').
 braches=$(git branch -r | egrep -v 'master' | cut -c 10-)
 
-for branch in $braches; 
-	do
-		echo "======================================================================="
-		echo "Branch: " $branch
-		git checkout $branch
-   		git merge master
-		if [[ $? == 1 ]]; then
-			echo "Encontrado conflito na $branch. Necessário resolver manualmente."  
-			$(git merge --abort)
-		else
-			echo "Merge realizado com sucesso;"
-			$(git push)
-		fi
-	done
-		echo "======================================================================="
+for branch in $braches; do
+  #Faz checkout na branch
+  git checkout $branch
 
-#Faz checkout da "master" 
+  #Faz merge com a master
+  git merge master --no-edit
+
+  #Se saída for igual a '1' informa o conflito e aborta o merge, se diferente de '1' informa o sucesso e realiza o push para a branch remota.
+  if [[ $? == 1 ]]; then
+    echo -e "\n[ERRO] Encontrado conflito na $branch. Necessário resolver manualmente."
+    $(git merge --abort)
+    #Coloca insere as branches com conflito no array $conflitos
+    conflitos+=($branch)
+  else
+    echo -e "\n[OK] Merge realizado com sucesso."
+    $(git push)
+    semconflitos+=($branch)
+  fi
+  
+  echo -e "\n=======================================================================\n"
+done
+
+echo -e "\n======================================================================="
+echo "Deleta as branches locais (exceto a 'master')"
+echo -e "=======================================================================\n"
 git checkout master
-
-#Deleta as branches locais (exceto a "master")
 git branch | egrep -v 'master' | xargs git branch -D
 
+function printbranches() {
+    name=$1[@]
+    txt=$2
+    arr=("${!name}")
+
+	echo -e "\n======================================================================="
+	echo -e "Branches $txt conflitos:"
+	echo -e "=======================================================================\n"
+    for branch in "${arr[@]}" ; do
+        echo "$branch"
+    done
+}
+
+printbranches semconflitos 'SEM'
+printbranches conflitos 'COM'
+
+echo -e "\n=======================================================================\n"
 set -x
